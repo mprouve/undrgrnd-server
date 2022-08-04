579fde511118a7523df959199f51f4c4a180bc49
Marco Prouve <marcoprouve@gmail.com>
2022-08-04 14:53:02 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# THE UNDRGRND
+
+## Getting Started with React
+
+This React project was bootstrapped with [Vite].
---
+++ server.js
@@ -0,0 +1,201 @@
+require("dotenv").config() // Load .env variables
+
+const express = require("express")
+const compression = require("compression")
+const cors = require("cors")
+const bodyParser = require("body-parser") // Parsing body of incoming requests
+const morgan = require("morgan")
+const path = require("path")
+const colors = require("colors")
+const config = require("./config.js") // Config variables
+const app = express() // INITIALIZE EXPRESS APP HERE
+
+// ***********************************************************
+// BEGIN: MIDDLEWARE *****************************************
+// ***********************************************************
+
+// MORGAN REQUEST LOGGING:
+app.use(morgan("dev"))
+
+// BODY PARSER:
+app.use(bodyParser.urlencoded({ extended: true })) // Allow 'application/x-www-form-urlencoded'
+app.use(
+  bodyParser.json({
+    verify: (request, response, buf) => {
+      request.rawBody = buf
+    },
+  })
+)
+
+// Begin logging in middleware
+app.use((request, response, next) => {
+  console.log(
+    colors.white.bold(
+      "---------------------------------------------------------------------"
+    )
+  )
+
+  next()
+})
+
+// CACHE HEADER CONTROL MIDDLEWARE
+// app.use((req, res, next) => {
+//   // here you can define period in second, this one is 5 minutes
+//   const period = 60 * 60 * 24 * 365 // 31536000 (1 year)
+
+//   // you only want to cache for GET requests
+//   if (req.method == 'GET') {
+//     res.set('Cache-control', `public, max-age=${period}`)
+//   } else {
+//     // for the other requests set strict no caching parameters
+//     res.set('Cache-control', `no-store`)
+//   }
+
+//   // remember to call next() to pass on the request
+//   next()
+// })
+
+// COMPRESSION MIDDLEWARE: Compresses responses to greatly decrease downloadable data by users from server
+app.use(compression())
+
+// CORS MIDDLEWARE:
+if (config.env !== "local") {
+  const whiteListDomains = [
+    "http://theundrgrnd-app.herokuapp.com",
+    "https://theundrgrnd-app.herokuapp.com",
+    "http://www.theundrgrnd.com",
+    "https://www.theundrgrnd.com",
+    "http://www.theundrgrnd.xyz",
+    "https://www.theundrgrnd.xyz",
+    "http://theundrgrnd.com",
+    "https://theundrgrnd.com",
+    "http://theundrgrnd.xyz",
+    "https://theundrgrnd.xyz",
+  ]
+
+  app.use(
+    cors({
+      origin(origin, callback) {
+        console.log(
+          colors.brightWhite(`REQUEST ORIGIN: ${origin}<${typeof origin}>`)
+        )
+        // If we want to allow requests with no origin uncomment below line
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true)
+        if (whiteListDomains.indexOf(origin) !== -1) {
+          return callback(null, true)
+        }
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin."
+        return callback(new Error(msg), false)
+      },
+    })
+  )
+} else {
+  app.use(cors())
+}
+
+// SECURE REDIRECT MIDDLEWARE (For Forcing https - when SSL enabled)
+app.use((request, response, next) => {
+  if (
+    config.env === "local" ||
+    request.headers["x-forwarded-proto"] === "https"
+  ) {
+    next()
+  } else {
+    console.log(colors.white("[REDIRECT]: ", "Redirecting to secure (HTTPS)"))
+
+    response.redirect(301, `https://${request.hostname}${request.originalUrl}`)
+  }
+})
+
+// LOGGER MIDDLEWARE:
+// Middleware to log request info and timestamp
+app.use((request, response, next) => {
+  console.log(colors.white("[URL]: ", request.url))
+  console.log(colors.grey("[TIME]: ", Date(Date.now()).toString()))
+
+  next()
+})
+
+// STATIC FILES:
+// Note the custom cache headers using the options object param in express.static
+app.use(
+  express.static(path.join(__dirname, config.app.public_dir), {
+    etag: true, // Just being explicit about the default.
+    lastModified: true, // Just being explicit about the default.
+    setHeaders: (res, path) => {
+      const hashRegExp = new RegExp("\\.[0-9a-f]{8}\\.")
+
+      if (path.endsWith(".html")) {
+        // All of the project's HTML files end in .html
+        res.setHeader("Cache-Control", "no-cache")
+      } else if (hashRegExp.test(path)) {
+        // If the RegExp matched, then we have a versioned URL.
+        res.setHeader("Cache-Control", "max-age=31536000")
+      }
+    },
+  })
+)
+
+// ROUTES ************************************
+// Routes that should handle incoming requests
+// *******************************************
+// const xRoutes = require('./controllers/x/routes/x.js')
+// app.use('/api/v1/x', xRoutes)
+// app.get('/api/v1/', (req, res) => res.status(200).json({ status: 1, message: 'ok' }))
+
+// CATCH ALL UNHANDLED GETS TO RENDER CLIENT ON URL INPUT
+app.get("/*", (request, response, next) => {
+  console.log(
+    colors.cyan(
+      "[NOTICE]: ",
+      "Using catch-all route handler - Returning entry file."
+    )
+  )
+
+  response.sendFile(path.join(__dirname, config.app.entry_file))
+})
+
+// ERROR HANDLING:
+// Middleware to catch unhandled requests
+app.use((request, response, next) => {
+  const error = new Error("Not found")
+  error.status = 404
+
+  next(error)
+})
+
+// Middleware to pass down all other errors not caught
+app.use((error, request, response) => {
+  console.log(colors.red.bold(`[ERROR]:  ${error.message}`))
+
+  response.status(error.status || 500).json({
+    error: {
+      message: error.message,
+    },
+  })
+})
+
+// ***********************************************************
+// END: MIDDLEWARE *******************************************
+// ***********************************************************
+
+// Improve debugging
+process.on("unhandledRejection", (reason, p) => {
+  console.log(
+    colors.red.bold(
+      "[ERROR]: ",
+      "Unhandled Rejection at:",
+      p,
+      "Reason:",
+      reason
+    )
+  )
+})
+
+app.listen(config.app.port, () => {
+  console.log(
+    colors.green.underline(`Server listening on port ${config.app.port}`)
+  )
+})
